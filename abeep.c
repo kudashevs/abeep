5f1d3f8a46919b5fb309a13190979c271a1053d6
Sergey Kudashev <kudashevs@gmail.com>
2026-05-05 22:55:26 +0200

    style: apply code formatting

--- abeep.c
+++ abeep.c
@@ -1,8 +1,10 @@
 #include <alsa/asoundlib.h>
 #include <assert.h>
+#include <math.h>
+#include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <unistd.h>
 
 #define PCM_DEFAULT "default"
 #define SAMPLE_RATE 8000
@@ -16,51 +18,53 @@
  * @param duration Beep duration in ms.
  */
 void beep(float freq, int duration) {
-	snd_pcm_t *playback_handle;
+  snd_pcm_t* playback_handle;
 
-	if(snd_pcm_open(&playback_handle, PCM_DEFAULT, SND_PCM_STREAM_PLAYBACK, 0) < 0) {
-		perror("Can't connect to the default interface");
-		exit(EXIT_FAILURE);
-	}
+  if (snd_pcm_open(&playback_handle, PCM_DEFAULT, SND_PCM_STREAM_PLAYBACK, 0) <
+      0) {
+    perror("Can't connect to the default interface");
+    exit(EXIT_FAILURE);
+  }
 
-	if (snd_pcm_set_params(playback_handle, SND_PCM_FORMAT_U8, SND_PCM_ACCESS_RW_INTERLEAVED, 1, SAMPLE_RATE, 1, 1000000) < 0) {
-		perror("Can't set connection parameters");
-		exit(EXIT_FAILURE);
-	}
+  if (snd_pcm_set_params(playback_handle, SND_PCM_FORMAT_U8,
+                         SND_PCM_ACCESS_RW_INTERLEAVED, 1, SAMPLE_RATE, 1,
+                         1000000) < 0) {
+    perror("Can't set connection parameters");
+    exit(EXIT_FAILURE);
+  }
 
-	assert(freq != 0);
-	assert(duration != 0);
+  assert(freq != 0);
+  assert(duration != 0);
 
-	int samples = (SAMPLE_RATE * duration) / 1000;
-	if (samples > MAX_SAMPLES) {
-		perror("Number of samples exceeds safety limits");
-		exit(EXIT_FAILURE);
-	}
+  int samples = (SAMPLE_RATE * duration) / 1000;
+  if (samples > MAX_SAMPLES) {
+    perror("Number of samples exceeds safety limits");
+    exit(EXIT_FAILURE);
+  }
 
-	uint8_t *buffer = malloc(samples);
-	if (buffer == NULL) {
-		perror("Can't allocate necessary memory");
-		exit(EXIT_FAILURE);
-	}
+  uint8_t* buffer = malloc(samples);
+  if (buffer == NULL) {
+    perror("Can't allocate necessary memory");
+    exit(EXIT_FAILURE);
+  }
 
+  for (int i = 0; i < samples; i++) {
+    buffer[i] = (uint8_t)(128 + 127 * sin(2 * M_PI * freq * i / SAMPLE_RATE));
+  }
 
-	for (int i = 0; i < samples; i++) {
-		buffer[i] = (uint8_t)(128 + 127 * sin(2 * M_PI * freq * i / SAMPLE_RATE));
-	}
+  if (snd_pcm_writei(playback_handle, buffer, samples) < 0) {
+    perror("Can't write to the connection");
+    snd_pcm_close(playback_handle);
+    exit(EXIT_FAILURE);
+  }
 
-	if (snd_pcm_writei(playback_handle, buffer, samples) < 0) {
-		perror("Can't write to the connection");
-		snd_pcm_close(playback_handle);
-		exit(EXIT_FAILURE);
-	}
-
-	free(buffer);
-	snd_pcm_drain(playback_handle);
-	snd_pcm_close(playback_handle);
+  free(buffer);
+  snd_pcm_drain(playback_handle);
+  snd_pcm_close(playback_handle);
 }
 
-int main() {
-	beep(FREQUENCY, DURATION);
+int main(int argc, char** argv) {
+  beep(FREQUENCY, DURATION);
 
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
