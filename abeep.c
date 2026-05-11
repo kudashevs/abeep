d163a71485fc2de58a4a7bfe523ac47b6e7f62d9
Sergey Kudashev <kudashevs@gmail.com>
2026-05-11 17:26:56 +0200

    refactor: reorder options

--- abeep.c
+++ abeep.c
@@ -228,7 +228,7 @@ int main(int argc, char** argv) {
   unsigned int flags = 0;
   int opt;
 
-  while ((opt = getopt(argc, argv, ":f:r:l:siVh")) != EOF) {
+  while ((opt = getopt(argc, argv, ":f:r:l:sihV")) != EOF) {
     switch (opt) {
       case 'f':
         float freq_candidate = atof(optarg);
