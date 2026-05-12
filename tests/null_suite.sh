1ff7299fd326c62e4d3d0a04fa9ed05e559d7c8b
Sergey Kudashev <kudashevs@gmail.com>
2026-05-12 17:19:14 +0200

    test: add null device suite

---
+++ tests/null_suite.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+TESTS_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+
+pushd ${TESTS_DIR} || exit 1
+
+if [ -z "$GITHUB_ACTIONS" ]; then
+    echo "Running locally..."
+    ( cd .. && make clean && make build )
+fi
+
+if [ $? -ne 0 ]; then
+    echo "build failed"
+    popd
+    exit $?
+fi
+
+echo ""
+
+stdbuf -oL -eL ../abeep -d null -f 440 -r 44100 -l 40 2>&1 | ./verify.sh -t storage/null-options
+
+stdbuf -oL -eL ../abeep -d null -f 440 -r 44100 -l 40 -v 2>&1 | ./verify.sh -t storage/null-options-verbose
+
+stdbuf -oL -eL ../abeep -d null -i 2>&1 | ./verify.sh -t storage/null-info
+
+stdbuf -oL -eL ../abeep -d null -i -v 2>&1 | ./verify.sh -t storage/null-info-verbose
+
+popd
